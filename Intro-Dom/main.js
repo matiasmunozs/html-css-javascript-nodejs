17215cfdbec7d9f9d63f2b6b7094eded87a19f02
matiasmunozs <chmunozs@gmail.com>
2023-08-23 23:42:32 -0400

    update

---
+++ Intro-Dom/main.js
@@ -0,0 +1,18 @@
+const todos = [];
+
+window.onload = ()=>{
+    const form = document.getElementById('todo-form');
+    form.onsubmit = (e) =>{
+        e.preventDefault()
+        const todo = document.getElementById('todo');
+        const todoText = todo.value;
+        todo.value = '';
+        todos.push(todoText)
+        console.log(todoText);
+        const todoList = document.getElementById('todo-list')
+        const todosTemplate = todos.map(t=>'<li>'+t+'</li>')
+        todoList.innerHTML = todosTemplate.join('')
+        document.querySelectorAll('#todo-list')
+    }
+
+}
